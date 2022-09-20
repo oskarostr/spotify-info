e61721880fabe291baa5a70dca06c388190b3042
Oskar <ostrowskioskar@gmail.com>
2022-09-20 23:26:46 +0200

    adding animation on scroll to recently played cards

--- src/components/RecentlyPlayed.js
+++ src/components/RecentlyPlayed.js
@@ -4,35 +4,35 @@ import LatestCard from "./RecentlyPlayedCard"
 function RecentlyPlayed(props) {
 
     let recentlyPlayedElements
-    if(props.recentlyPlayed) {
-    recentlyPlayedElements = props.recentlyPlayed.map(last => {
-        //creating new string with artist names
-        let artistsName = last.track.artists[0].name
-        for(let i = 1; i < last.track.artists.length; i++) {
-            //console.log(i + ' ' + last.artists[i].name)
-            artistsName += ', ' + last.track.artists[i].name
-        }
+    if (props.recentlyPlayed) {
+        recentlyPlayedElements = props.recentlyPlayed.map(last => {
+            //creating new string with artist names
+            let artistsName = last.track.artists[0].name
+            for (let i = 1; i < last.track.artists.length; i++) {
+                //console.log(i + ' ' + last.artists[i].name)
+                artistsName += ', ' + last.track.artists[i].name
+            }
 
-        //counting time from last played
-        const date = new Date()
+            //counting time from last played
+            const date = new Date()
 
-        return (
-            <LatestCard 
-                key = {last.track.id} 
-                name = {last.track.name}
-                image = {last.track.album.images[1].url}
-                artist = {artistsName}
+            return (
+                <LatestCard
+                    key={last.track.id}
+                    name={last.track.name}
+                    image={last.track.album.images[1].url}
+                    artist={artistsName}
                 //time = {minutes}
-            />
-        )
-    })
+                />
+            )
+        })
     }
 
     return (
         <div className='recently-played__content-container'>
-            <Header 
-                userName = {props.userName}
-                text = ', these are your recently played songs'
+            <Header
+                userName={props.userName}
+                text=', these are your recently played songs'
             />
             <div className="recently-played__elements-container">
                 {recentlyPlayedElements}
--- src/components/RecentlyPlayedCard.js
+++ src/components/RecentlyPlayedCard.js
@@ -1,9 +1,32 @@
 import '../styles/imports/recently-played-card.scss'
-import { GiBackwardTime } from "react-icons/gi";
+import { useEffect } from 'react';
+//import { GiBackwardTime } from "react-icons/gi";
+
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+const squareVariants = {
+    visible: { opacity: 1, scale: 1, transition: { duration: 0.5 }, x: 0 },
+    hidden: { opacity: 0, scale: 0, x: -1000 }
+  };
 
 function LatestCard(props) {
+    const controls = useAnimation();
+    const [ref, inView] = useInView();
+    useEffect(() => {
+      if (inView) {
+        controls.start("visible");
+      }
+    }, [controls, inView]);
+
     return (
-        <div className="recently-played__card">
+        <motion.div 
+            className="recently-played__card"
+            variants={squareVariants}
+            ref={ref}
+            animate={controls}
+            initial="hidden"
+        >
             <img src = {props.image} alt='song cover'/>
             <div className="info-box">
                 <h1>{props.name}</h1>
@@ -13,7 +36,7 @@ function LatestCard(props) {
                 <GiBackwardTime className='icon'/>
                 <p className='time'>{props.time}m</p>
             </div>*/}
-        </div>
+        </motion.div>
     )
 }
 
