336046576bd2a5aa522f67cc073774a20996d1f5
Oskar <ostrowskioskar@gmail.com>
2022-09-23 20:37:18 +0200

    highlighting currently active page in sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -9,12 +9,12 @@ function SideBar(props) {
                 <h1>Spotify</h1>
             </div>
             <ul className='nav-list'>
-                <div className='nav-item' onClick={props.changeToTracks}>
+                <div className='nav-item' onClick={props.changeToTracks} style={{color: props.page === 'tracks' ? '#1DB954' : ''}}>
                     <IoIosPodium className='icon'/>
                     <li className='nav-text'>Top tracks</li>
                 </div>
 
-                <div className='nav-item' onClick={props.changeToRecently}>
+                <div className='nav-item' onClick={props.changeToRecently} style={{color: props.page === 'recently' ? '#1DB954' : ''}}>
                     <BiTimer className='icon' />
                     <li className='nav-text'>Recently played</li>
                 </div>
